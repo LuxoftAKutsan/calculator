cda88c4499e8c2170ef5d62a99afed4b04c0c4f3
Serhii Zaporozhchenko <serhii.zaporozhchenko@gmail.com>
2019-01-24 14:33:19 +0200

    Init

---
+++ calculator.cpp
@@ -0,0 +1,121 @@
+#include "calculator.hpp"
+
+std::vector<std::string> split(const std::string& s, char delimiter)
+{
+   std::vector<std::string> tokens;
+   std::string token;
+   std::stringstream tokenStream(s);
+   while (std::getline(tokenStream, token, delimiter))
+   {
+      tokens.push_back(token);
+   }
+   return tokens;
+}
+
+Result calculateExpression(const std::string & expr)
+{
+    if(true == expr.empty())
+    {
+        return Result{0.0, Error::EmptyString};
+    }
+
+    auto tokens = split(expr, ' ');
+    std::string operation = "";
+    double operand1 = 0.0;
+    double operand2 = 0.0;
+    Result res = {0.0, Error::NoError};
+    StackState state = StackState::WaitingFirstOperand;
+
+    for (auto token = tokens.begin(); token != tokens.end(); ++token)
+    {
+        if(Error::NoError == res.error)
+        {
+            if(StackState::WaitingFirstOperand == state)
+            {
+                if(true == isValidOperand(*token))
+                {
+                    operand1 = std::stod(*token);
+                    state = StackState::WaitingSecondOperand;
+                }
+                else
+                {
+                    return {0.0, Error::WrongOperand};
+                }
+            }
+            else if (StackState::WaitingSecondOperand == state)
+            {
+                if(true == isValidOperand(*token))
+                {
+                    operand2 = std::stod(*token);
+                    state = StackState::WaitingOperation;
+                }
+                else
+                {
+                    return {0.0, Error::WrongOperand};
+                }
+            }
+            else if (StackState::WaitingOperation)
+            {
+                if(true == isValidOperation(*token))
+                {
+                    operation = *token;
+                    res = calculateOperation(operand1, operand2, operation);
+                    operand1 = res.value;
+                    operand2 = 0.0;
+                    state = StackState::WaitingSecondOperand;
+                }
+                else
+                {
+                    return {0.0, Error::WrongOperation};
+                }
+            }
+        }
+        else
+        {
+            return res;
+        }
+    }
+    return res;
+}
+
+Result calculateOperation(double operand1, double operand2, const std::string& operation)
+{
+    Result res = {0.0, Error::NoError};
+    if ("+" == operation)
+    {
+        res.value = operand1 + operand2;
+    }
+    else if("-" == operation)
+    {
+        res.value = operand1 - operand2;
+    }
+    else if("*" == operation)
+    {
+        res.value = operand1 * operand2;
+    }
+    else if("/" == operation)
+    {
+        if(0.0 == operand2)
+        {
+            return {0.0, Error::DivideByZero};
+        }
+        else
+        {
+            res.value = operand1 / operand2;
+        }
+    }
+    return res;
+}
+
+bool isValidOperation(const std::string & operation)
+{
+    std::regex r("[\+\-\/\*]");
+    return std::regex_match(operation, r);
+}
+
+bool isValidOperand(const std::string & operand)
+{
+    std::regex r("[-+]?[0-9]*\.?[0-9]+");
+    return std::regex_match(operand, r);
+}
+
--- calculator.hpp
+++ calculator.hpp
@@ -1,26 +1,24 @@
 #ifndef CALCULATOR_HPP
-#define CALCULATOR_HPP
+#define CALCUALTOR_HPP
 
 #include <string>
+#include <vector>
+#include <stack>
+#include <sstream>
+#include <regex>
+#include <limits>
+#include "stdlib.h"
 
-namespace CalculatorPolish
-{
+enum StackState{WaitingFirstOperand, WaitingSecondOperand, WaitingOperation, ReadyToCalculate, Invalid};
+enum Error{NoError, DivideByZero, EmptyString, WrongOperand, WrongOperation, OperandOverflow};
+typedef struct{double value; Error error;} Result;
 
-    class Calculator
-    {
-    public:
-        /**
-         * @brief calculate expression read from given string
-         * @param expression to calculate
-         * @return value of calculated expression
-         */
-        double calculate(const std::string& expression);
+std::vector<std::string> split(const std::string& s, char delimiter);
 
-    private:
-        const static int nullTerminatorLength = 1;
-        const static int firstChar = 0;
-    };
+Result calculateExpression(const std::string & expr);
+Result calculateOperation(double operand1, double operand2, const std::string& operation);
 
-}
+bool isValidOperation(const std::string & operation);
+bool isValidOperand(const std::string & operand);
 
 #endif
